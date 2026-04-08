8b78ea42d406c00e73c734ae0b62010f236d1526
Archinime <archinime12@gmail.com>
2026-04-08 09:11:25 -0500

    Add files via upload

---
+++ musica-data.js
@@ -0,0 +1,375 @@
+const audioPlaylists = {
+    "1": [
+        "dandadan.mp3"
+    ],
+    "2": [
+        "jujutsukaisen.mp3"
+    ],
+    "3": [
+        "konosuba.mp3"
+    ],
+    "4": [
+        "「Call of the Witch」ReZero OST 15 ／『Main Soundtrack』.mp3",
+        "ReZERO - Starting Life in Another World - Ending.mp3",
+        "ReZERO -Starting Life in Another World- Season 2 - Ending Memento.mp3"
+    ],
+    "5": [
+        "mushokutensei.mp3"
+    ],
+    "6": [
+        "tenseislime.mp3"
+    ],
+    "7": [
+        "tayna.mp3"
+    ],
+    "8": [
+        "akame.mp3"
+    ],
+    "9": [
+        "tobeherox.mp3"
+    ],
+    "10": [
+        "demonslayer1.mp3",
+        "demonslayer2.mp3",
+        "demonslayer3.mp3"
+    ],
+    "11": [
+        "maou2099.mp3"
+    ],
+    "12": [
+        "Wistoria Wand and Sword - Opening  Fire and Fear.mp3"
+    ],
+    "13": [
+        "izure.mp3"
+    ],
+    "14": [
+        "alyarusa.mp3"
+    ],
+    "15": [
+        "Rebornasa vending.mp3"
+    ],
+    "16": [
+        "Kimi to Boku no Saigo.mp3"
+    ],
+    "17": [
+        "The Gorilla Gods Go- To Girl  illuminate - Nakamura Shuugo「KanRomPer」.mp3"
+    ],
+    "18": [
+        "Chainsaw Man – The Movie Reze Arc - Theme Song FULL IRIS OUT by Kenshi Yonezu (Lyrics).mp3"
+    ],
+    "19": [
+        "MADKID  RISE (The Rising of the Shield Hero Opening Theme).mp3"
+    ],
+    "20": [
+        "『Hinooka Rei - Uzuho』 Overflow Ending Full  ϟ Sub. Español - Romaji ♡.mp3"
+    ],
+    "21": [
+        "Shiunji-ke no Kodomo-tachi Opening Full  Honey Lemon - Sub. Español ♡.mp3"
+    ],
+    "22": [
+        "Goblin Slayer OP Full  Rightfully - Sub Español.mp3"
+    ],
+    "23": [
+        "Unaware Atelier Meister - Opening full FACTSORY by MeseMoa [Lyrics].mp3"
+    ],
+    "24": [
+        "Onii-chan no Koto Nanka Zenzen Suki Janain Dakara Ne!!  ED FULL.mp3"
+    ],
+    "25": [
+        "アニメ『怪獣８号』ノンクレジットED｜OneRepublic「Nobody」｜毎週土曜23時～放送・配信.mp3"
+    ],
+    "26": [
+        "Zootopia - Try Everything By Shakira (Music Video).mp3"
+    ],
+    "27": [
+        "clevates.mp3"
+    ],
+    "28": [
+        "https://cdn.jsdelivr.net/gh/Archinime/imagenes@main/stone1.mp3",
+        "https://cdn.jsdelivr.net/gh/Archinime/imagenes@main/stone4.mp3"
+    ],
+    "29": [
+        "We Are. OP. 1  One Piece.  Traduccion Al Español.mp3"
+    ],
+    "30": [
+        "Shoujo Ramune (Anime) Opening (小女ラムネ OVA OP - 挿入歌 ).mp3"
+    ],
+    "31": [
+        "To Your Eternity Opening  Hikaru Utada -  Pink Blood  Sub Español - Lyrics Romaji.mp3"
+    ],
+    "32": [
+        "Tsugunai Subbed.mp3"
+    ],
+    "33": [
+        "Blue Lock OP Full  Chaos ga Kiwamaru  Sub Español - Romaji『AMV』☆.mp3"
+    ],
+    "34": [
+        "Magic Maker Isekai Mahou no Tsukurikata ending full  Yoake no Uta (夜明けの歌) by Humbreaders.mp3"
+    ],
+    "35": [
+        "New Saga Ending - Her with KanRomEn.mp3"
+    ],
+    "36": [
+        "Gachiakuta - Ending FULL TOMOSHIBI by DUSTCELL (Lyrics).mp3"
+    ],
+    "37": [
+        "Ending Shinsei Kourin Dacryon.mp3"
+    ],
+    "38": [
+        "Kami Tachi ni Hirowareta Otoko OP  Azusa Tadokoro - Yasashii Sekai Sub Español, Romaji & English.mp3"
+    ],
+    "39": [
+        "Pyramid Daigyakuten - by Spira Spica - Ore dake Haireru Kakushi Dungeon Opening.mp3"
+    ],
+    "40": [
+        "One punch man - The Hero  OP 1  sub. español ; lyrics ; letra.mp3"
+    ],
+    "41": [
+        "Bling-Bang-Bang-Born.mp3"
+    ],
+    "42": [
+        "Kusuriya no Hitorigoto ed 1.mp3",
+        "Kusuriya no Hitorigoto Season 2 Opening Full  Hyakka Ryouran by Lilas Ikuta - Sub. Español ♡.mp3",
+        "Kusuriya no Hitorigoto 3.mp3"
+    ],
+    "43": [
+        "phub.mp3"
+    ],
+    "44": [
+        "Uzumaki  Opening theme Song   Intro Anime.mp3"
+    ],
+    "45": [
+        "Tondemo Skill de Isekai Hourou Meshi Opening Full  Luxury Spoon -  Van de Shop  Sub Español Latino.mp3"
+    ],
+    "46": [
+        "My Dress-Up Darling Season 2 Opening Theme).mp3"
+    ],
+    "47": [
+        "Poison (Español Latino) - Hazbin Hotel Fan Lyric Vídeo.mp3"
+    ],
+    "48": [
+        "The Eminence in Shadow  Moonlight Sonata (1st Movement) - Beethoven.mp3",
+        "The Eminence in Shadow Season 2 OP Full  grayscale dominator  Sub Español - Romaji『AMV』☆.mp3"
+    ],
+    "49": [
+        "Spy x Family Ed. 2 Full  Shikisai by yama  Sub Español.mp3"
+    ],
+    "50": [
+        "Black Clover.mp3"
+    ],
+    "51": [
+        "Boku no Hero Academia Season 1 Ending 1 full『 Brian the Sun - Heroes』【Sub EspañolJaponés】AMV.mp3"
+    ],
+    "52": [
+        "Kakou no iinazuke OP FULL  Kiyoe Yoshioka Dekoboko  Sub Español, Romaji & English-AMV.mp3"
+    ],
+    "53": [
+        "futari solo camp.mp3"
+    ],
+    "54": [
+        "Tojima Tanzaburo Wants To Be A Kamen Rider OP FULL - 『Wanna Be』 by TeddyLoid feat. Shigeru Matsuzaki.mp3"
+    ],
+    "55": [
+        "Alma-chan wa Kazoku ni Naritai Ending Full『Arifuretetai』by KAF.mp3"
+    ],
+    "56": [
+        "Spice and Wolf (2024) ED  Ending 2 - Creditless  4K  24FPS (1).mp3"
+    ],
+    "57": [
+        "【Oyasumi Sekusu Episode 4】Ending (1).mp3"
+    ],
+    "58": [
+        "rym.mp3"
+    ],
+    "59": [
+        "akujiki.mp3"
+    ],
+    "60": [
+        "saigonihitotsu.mp3"
+    ],
+    "61": [
+        "syn.mp3",
+        "syn2.mp3"
+    ],
+    "62": [
+        "record.mp3"
+    ],
+    "63": [
+        "phub.mp3"
+    ],
+    "64": [
+        "gnosia.mp3"
+    ],
+    "65": [
+        "classroomelite.mp3"
+    ],
+    "66": [
+        "frieren.mp3"
+    ],
+    "67": [
+        "sololeveling.mp3"
+    ],
+    "68": [
+        "it.mp3"
+    ],
+    "69": [
+        "cars.mp3"
+    ],
+    "70": [
+        "kimitokoete.mp3"
+    ],
+    "71": [
+        "overlord.mp3"
+    ],
+    "72": [
+        "https://www.dropbox.com/scl/fi/b9hd9pve2m5bb4lt8kbfx/Seihantai-na-Kimi-To-Boku-Opening-Megane-wo-Hazushite-by-Noa-gQtMev-oeus.mp3?rlkey=jrqq4ev3p5mlix9l4odjo5zlh&st=jwgf139z&raw=1"
+    ],
+    "73": [
+        "https://www.dropbox.com/scl/fi/ts8eutbg0xpzv4fx6jzd8/Sentenced-to-Be-a-Hero-Opening-Kill-the-Noise-with-kanRomEn.mp3?rlkey=5ldhfiyth2mf6s4mdir95ka3u&st=g2f3vdwe&raw=1"
+    ],
+    "74": [
+        "https://www.dropbox.com/scl/fi/t4ldbl0zgb2e7if44bs4r/Shibou-Yuugi-de-Meshi-wo-Kuu-OP-Full-Hikari-e-Hashire-TV.mp3?rlkey=azeb2akcdtciomenr3upn4kii&st=tp2wxrl6&raw=1"
+    ],
+    "75": [
+        "https://www.dropbox.com/scl/fi/vtc97fwkrlczovfdke95z/I-swear.mp3?rlkey=4ltjfx1xizi9ub24u8epkd8qq&st=h1hynhwo&raw=1"
+    ],
+    "76": [
+        "https://www.dropbox.com/scl/fi/nmtq5a2n2ipoognughtr5/Lets-Play-1-2-Play.mp3?rlkey=24qh32l9mgt5xdn8s2l78y4d2&st=sorxydgl&raw=1"
+    ],
+    "77": [
+        "https://www.dropbox.com/scl/fi/mgfvc0w16noqdh4a12ts5/Mrs.-GREEN-APPLE-Inferno.mp3?rlkey=7w7lenfoaplou65lu0vycsexg&st=p1ijtaxo&raw=1"
+    ],
+    "78": [
+        "https://www.dropbox.com/scl/fi/fikl8mp1ht47xmzipjgxa/Hana-Kimi-Opening-Adrena-by-YOASOBI.mp3?rlkey=94ano4j50v8nhd492j9j2uw42&st=lnjja3mi&raw=1"
+    ],
+    "79": [
+        "https://www.dropbox.com/scl/fi/4ngz61g7k9s87o2rb2q3f/DARK-MOON-ENHYPEN-One-In-A-Billion-Japanese-Ver..mp3?rlkey=e8jj4bofrtrbi9gio0oqto2zc&st=lg79kept&raw=1"
+    ],
+    "80": [
+        "https://www.dropbox.com/scl/fi/u7v0jl0iipu8cf7ll694a/Jigokuraku-OP.-Full-WORK-Sub.-Espa-ol-AMV.mp3?rlkey=pzsy2kth2ibchlgv3qtzinelb&st=707mmoc0&raw=1"
+    ],
+    "81": [
+        "https://www.dropbox.com/scl/fi/w5j4gmrjtjmtkcgi8eo3q/NieRAutomata-Ver1.1a-Season-2-Ending-Full-Hai-to-Inori-Sub-Espa-ol-AMV.mp3?rlkey=1fmp1g26rlnvjr1wozqskgvjs&st=s70ilkpy&raw=1"
+    ],
+    "82": [
+        "https://www.dropbox.com/scl/fi/3vfig1wo2afaq4ybyg423/Tsuki-ga-Michibiku-Isekai-Douchuu-Season-2-OP.-2-Full-Sub-espa-ol-Rom-Reversal-Syudou.mp3?rlkey=tq0kcg5gnxsqjm333ftcxbfgz&st=7ums4zwn&raw=1"
+    ],
+    "83": [
+        "https://cdn.jsdelivr.net/gh/Archinime/imagenes@main/iruma.mp3",
+        "https://cdn.jsdelivr.net/gh/Archinime/imagenes@main/iruma1.mp3"
+    ],
+    "84": [
+        "https://www.dropbox.com/scl/fi/g16veupg68rihmd46j25c/Miss-Kobayashis-Dragon-Maid-Opening-FULL.mp3?rlkey=egmnpr90obkf6g4j85su9xsxk&st=s3g1w50y&raw=1"
+    ],
+    "85": [
+        "https://www.dropbox.com/scl/fi/z39v3kgx5m2mr8wbyguf4/CreditlessSword-Art-Online_-Alicization-OP-Opening-1UHD-60FPS.mp3?rlkey=kdt5874k5qyp2b7bwdkx9wifw&st=jq1anfiv&raw=1",
+        "https://www.dropbox.com/scl/fi/xjlx49d3l5x4htcqtbbkq/LiSAcrossing-field-1-1st-OP.mp3?rlkey=kr2aglsnhrmedetvkjenklt9i&st=rb90uskg&raw=1",
+        "https://www.dropbox.com/scl/fi/8557cbs60g8bgx8cc3to9/INNOCENCE-1-2nd-OP.mp3?rlkey=9ksl3tntdfb5l060ypol880c0&st=lc98yhwk&raw=1"
+    ],
+    "86": [
+        "https://www.dropbox.com/scl/fi/sulyu8dxvgrtbknd7rb5m/Baki-Season-1-OP1BEASTFUL-Full.mp3?rlkey=e88df44fb05nuif9po7oogejd&st=zd5aoot5&raw=1"
+    ],
+    "87": [
+        "https://www.dropbox.com/scl/fi/84s1v2l37yxlok5lz7h21/The-World.-Death-Note-OP.1-Traducido-Al-Espa-ol..mp3?rlkey=p0ijyzl9itnpepgrjyejllsos&st=4f2ie4nd&raw=1"
+    ],
+    "88": [
+        "https://www.dropbox.com/scl/fi/ksfx8464u2293vux1v8nf/Leviathan-OP-Paths-Combine-by-Joe-Hisaishi-Netflix-Anime.mp3?rlkey=yicku7wgtj8i3bj6tiyvlf2pt&st=4emorcv2&raw=1"
+    ],
+    "89": [
+        "https://www.dropbox.com/scl/fi/r34di8n2mvzsqcszw6yhn/seisyun-complex.mp3?rlkey=m7yri0tkoaaabbx0smbtqz05p&st=dtn4vgfd&raw=1"
+    ],
+    "90": [
+        "https://www.dropbox.com/scl/fi/g24d8bw9nv0vueg4yjraa/Shangri-la-Frontier-Season-2-Opening-2-Frontiers.mp3?rlkey=zonzeec8wap1cnfjy3e6b42ug&st=ord0ivng&raw=1",
+        "https://www.dropbox.com/scl/fi/gi53jsx8hz2eelzl3l15u/Shangri-la-Frontier-Opening-Broken-Games.mp3?rlkey=jbpo19u54pgbppdw0iat99h5i&st=t0m0vbxi&raw=1",
+        "https://www.dropbox.com/scl/fi/6q2yn5h8ap1buqtwtebpg/Shangri-la-Frontier-Season-2-Opening-Queen-by-LiSA.mp3?rlkey=qysjemi1tkbl2oci6vf6k42hy&st=r7kk105w&raw=1"
+    ],
+    "91": [
+        "https://www.dropbox.com/scl/fi/1y2wdh5m8py2tx4bod0z0/TRIGUN-STAMPEDE-OP.-Full-TOMBI-Sub.-Espa-ol-AMV.mp3?rlkey=eawwgvqac9i4rq29b19mqodi9&st=r9zvbxcw&raw=1",
+        "https://www.dropbox.com/scl/fi/xuvi2n4vagouh77jrhke5/Eng-LyricsConcept-Art.VerTRIGUN-STAMPEDE-Full-Ending-by-Salyu-Haruka-Nakamura.mp3?rlkey=2go145cxrv62p3a9bej8wz3aw&st=8q4x78uq&raw=1"
+    ],
+    "92": [
+        "https://www.dropbox.com/scl/fi/0zvi91ka303ok249u5wpx/Roll-Over-and-Die-Opening-Liberator-with-KanRomEn.mp3?rlkey=p2tbx1hsquc5zn9qp9lm8ywky&st=pc8vqkmf&raw=1",
+        "https://www.dropbox.com/scl/fi/g7xm04ivmp2cn25bbi5vx/Roll-Over-and-Die-Ending-I-need-with-KanRomEn.mp3?rlkey=lj6ouvwmd94wvurid38iow3gp&st=rpud5p98&raw=1"
+    ],
+    "93": [
+        "https://www.dropbox.com/scl/fi/aahlmgroyqa04fgpw9xhk/SLUMP-Ending-1.-Tower-of-God.-Subtitulado-al-Espa-ol..mp3?rlkey=qvn3wnudtnpr4c27rgu4kukcd&st=2y7f6hd6&raw=1",
+        "https://www.dropbox.com/scl/fi/qmq2e9j3g0oyknkhqcy1z/Tower-of-God-opening-Stray-Kids-TOP-Japanese-version-Sub-espa-ol.mp3?rlkey=6nu3vfqym23ym8rkv74jrl67t&st=9ihevgk4&raw=1"
+    ],
+    "94": [
+        "https://cdn.jsdelivr.net/gh/Archinime/imagenes@main/【推しの子】ノンクレジットオープニング｜YOASOBI「アイドル」.mp3",
+        "https://cdn.jsdelivr.net/gh/Archinime/imagenes@main/【推しの子】第3期ノンクレジットエンディング｜なとり「セレナーデ」.mp3",
+        "https://cdn.jsdelivr.net/gh/Archinime/imagenes@main/【推しの子】第3期ノンクレジットオープニング｜ちゃんみな「TEST%20ME」.mp3"
+    ],
+    "95": [
+        "https://cdn.jsdelivr.net/gh/Archinime/imagenes@main/gallo.mp3"
+    ],
+    "96": [
+        "https://cdn.jsdelivr.net/gh/Archinime/imagenes@main/Ms13.mp3",
+        "https://cdn.jsdelivr.net/gh/Archinime/imagenes@main/Ms132.mp3"
+    ],
+    "97": [
+        "https://cdn.jsdelivr.net/gh/Archinime/imagenes@main/winder.mp3",
+        "https://cdn.jsdelivr.net/gh/Archinime/imagenes@main/winder2.mp3"
+    ],
+    "98": [
+        "https://cdn.jsdelivr.net/gh/Archinime/imagenes@main/sonic.mp3"
+    ],
+    "99": [
+        "https://cdn.jsdelivr.net/gh/Archinime/imagenes@main/Peaches.mp3"
+    ],
+    "100": [
+        "https://cdn.jsdelivr.net/gh/Archinime/imagenes@main/komisan.mp3",
+        "https://cdn.jsdelivr.net/gh/Archinime/imagenes@main/komisanmusica2.mp3"
+    ],
+    "101": [
+        "https://cdn.jsdelivr.net/gh/Archinime/imagenes@main/bokunokokoro.mp3",
+        "https://cdn.jsdelivr.net/gh/Archinime/imagenes@main/bokunokokoro3.mp3"
+    ],
+    "102": [
+        "https://cdn.jsdelivr.net/gh/Archinime/imagenes@main/darwin.mp3",
+        "https://cdn.jsdelivr.net/gh/Archinime/imagenes@main/darwin2.mp3"
+    ],
+    "103": [
+        "https://cdn.jsdelivr.net/gh/Archinime/imagenes@main/horimiya1.mp3",
+        "https://cdn.jsdelivr.net/gh/Archinime/imagenes@main/horimiya2.mp3"
+    ],
+    "104": [
+        "https://cdn.jsdelivr.net/gh/Archinime/imagenes@main/doro1.mp3",
+        "https://cdn.jsdelivr.net/gh/Archinime/imagenes@main/doro2.mp3",
+        "https://cdn.jsdelivr.net/gh/Archinime/imagenes@main/doro3.mp3",
+        "https://cdn.jsdelivr.net/gh/Archinime/imagenes@main/doro4.mp3"
+    ],
+    "105": [
+        "https://cdn.jsdelivr.net/gh/Archinime/imagenes@main/ganbare.mp3",
+        "https://cdn.jsdelivr.net/gh/Archinime/imagenes@main/ganbare1.mp3"
+    ],
+    "106": [
+        "https://cdn.jsdelivr.net/gh/Archinime/imagenes@main/Rompiendoelhielo.mp3",
+        "https://cdn.jsdelivr.net/gh/Archinime/imagenes@main/Rompiendoelhielo1.mp3"
+    ],
+    "107": [
+        "https://cdn.jsdelivr.net/gh/Archinime/imagenes@main/snowball1.mp3",
+        "https://cdn.jsdelivr.net/gh/Archinime/imagenes@main/snowball2.mp3"
+    ],
+    "108": [
+        "https://cdn.jsdelivr.net/gh/Archinime/imagenes@main/rilakkuma.mp3"
+    ],
+    "109": [
+        "https://cdn.jsdelivr.net/gh/Archinime/imagenes@main/dawmons.mp3"
+    ],
+    "110": [
+        "https://cdn.jsdelivr.net/gh/Archinime/imagenes@main/Nippon.mp3"
+    ],
+    "111": [
+        "https://cdn.jsdelivr.net/gh/Archinime/imagenes@main/Atelier%20of%20Witch%20Hat.mp3"
+    ],
+    "112": [
+        "https://cdn.jsdelivr.net/gh/Archinime/imagenes@main/Vastness.mp3"
+    ],
+    "113": [
+        "https://cdn.jsdelivr.net/gh/Archinime/imagenes@main/bluebox1.mp3",
+        "https://cdn.jsdelivr.net/gh/Archinime/imagenes@main/bluebox2.mp3",
+        "https://cdn.jsdelivr.net/gh/Archinime/imagenes@main/bluebox3.mp3",
+        "https://cdn.jsdelivr.net/gh/Archinime/imagenes@main/bluebox4.mp3"
+    ]
+};
