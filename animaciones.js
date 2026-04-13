b603f1a56a6c2ee7c70b756f66bf03b123a7bd36
Archinime <archinime12@gmail.com>
2026-04-12 20:07:51 -0500

    Add files via upload

--- animaciones.js
+++ animaciones.js
@@ -2,6 +2,16 @@
    Lista de videos para efectos de pantalla verde/azul */
 
 const videoList = [
-  { id: 'hola', src: 'https://cdn.jsdelivr.net/gh/Archinime/Archivos-data@main/rem.mp4', preset: { threshold: 0.25, diff: 0, soft: 100 } },
-  { id: 'rem',  src: 'https://cdn.jsdelivr.net/gh/Archinime/Archivos-data@main/shinobu.mp4',  preset: { threshold: 0.10, diff: 0, soft: 100  } },
+  { id: 'hola', src: 'hola.mp4', preset: { threshold: 0.25, diff: 0, soft: 100 } },
+  { id: 'rem',  src: 'rem.mp4',  preset: { threshold: 0.10, diff: 0, soft: 100  } },
+
+  // Animaciones navideñas (pantalla verde)
+  { id: 'animacionnavidad1', src: 'animacionnavidad1.mp4', preset: { threshold: 0.10, diff: 0, soft: 100 }, keyColor: 'green' },
+  { id: 'animacionnavidad2', src: 'animacionnavidad2.mp4', preset: { threshold: 0.06, diff: 8, soft: 60, }, keyColor: 'green' },
+  { id: 'animacionnavidad3', src: 'animacionnavidad3.mp4', preset: { threshold: 0.10, diff: 0, soft: 100 }, keyColor: 'green' },
+  { id: 'animacionnavidad4', src: 'animacionnavidad4.mp4', preset: { threshold: 0.10, diff: 0, soft: 100 }, keyColor: 'green' },
+
+  // Animaciones navideñas (pantalla azul)
+  { id: 'animacionnavidad5', src: 'animacionnavidad5.mp4', preset: { threshold: 0.12, diff: 0, soft: 100 }, keyColor: 'blue' },
+  { id: 'animacionnavidad6', src: 'animacionnavidad6.mp4', preset: { threshold: 0.12, diff: 0, soft: 100 }, keyColor: 'blue' }
 ];
