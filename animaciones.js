f9b165dbba9d708b48499a18afcd6c1e7796b149
Archinime <archinime12@gmail.com>
2026-04-12 19:53:28 -0500

    Add files via upload

---
+++ animaciones.js
@@ -0,0 +1,7 @@
+/* Archivo: animaciones.js 
+   Lista de videos para efectos de pantalla verde/azul */
+
+const videoList = [
+  { id: 'hola', src: 'https://cdn.jsdelivr.net/gh/Archinime/Archivos-data@main/rem.mp4', preset: { threshold: 0.25, diff: 0, soft: 100 } },
+  { id: 'rem',  src: 'https://cdn.jsdelivr.net/gh/Archinime/Archivos-data@main/shinobu.mp4',  preset: { threshold: 0.10, diff: 0, soft: 100  } },
+];
