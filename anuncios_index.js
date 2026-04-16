845bcc5313baa699ab99bb1722e9e6020859022c
Archinime <archinime12@gmail.com>
2026-04-16 16:17:44 -0500

    Add files via upload

--- anuncios_index.js
+++ anuncios_index.js
@@ -8,7 +8,6 @@ const listaAnuncios = [
     id: 'mi_anuncio_300x250',
     red: 'HighPerformanceFormat',
     descripcion: 'Banner 300x250 - Código personalizado',
-    // Aquí pego EXACTAMENTE el código que me diste
     codigo: `
       <script>
         atOptions = {
@@ -22,8 +21,7 @@ const listaAnuncios = [
       <script src="https://www.highperformanceformat.com/c80e9060ab41f7adc2b2ccc358ef6c65/invoke.js"></script>
     `
   }
-  // Si en el futuro quieres añadir otro anuncio de otra red,
-  // solo tienes que agregar otro objeto aquí.
+  // Si quieres más anuncios, agrégalos aquí y la función aleatoria los elegirá
 ];
 
 // Función auxiliar para obtener el código de un anuncio por su ID
@@ -33,6 +31,6 @@ function obtenerHTMLAnuncio(anuncioId) {
   return anuncio.codigo;
 }
 
-// Hacemos accesibles las variables globalmente (para usarlas desde index.html)
+// Hacemos accesibles las variables globalmente
 window.listaAnuncios = listaAnuncios;
 window.obtenerHTMLAnuncio = obtenerHTMLAnuncio;
