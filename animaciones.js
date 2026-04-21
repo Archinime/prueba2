bf2ed71ba48934c4b76a24c05124f8cfb8ab5005
Archinime <archinime12@gmail.com>
2026-04-21 10:54:16 -0500

    Add files via upload

--- animaciones.js
+++ animaciones.js
@@ -5,7 +5,7 @@ const videoList = [
   { id: 'hola', src: 'hola.mp4', preset: { threshold: 0.25, diff: 0, soft: 100 } },
   { id: 'rem',  src: 'rem.mp4',  preset: { threshold: 0.10, diff: 0, soft: 100  } },
   { id: 'rimuru',  src: 'rimuru.mp4',  preset: { threshold: 0.10, diff: 0, soft: 100  } },
-  { id: 'mikasa',  src: 'mikasa.mp4',  preset: { threshold: 0.10, diff: 0, soft: 100  } },
+  { id: 'reze',  src: 'reze.mp4',  preset: { threshold: 0.10, diff: 0, soft: 100  } },
   { id: 'levi',  src: 'levi.mp4',  preset: { threshold: 0.10, diff: 0, soft: 100  } },
 
 ];
--- sw.js
+++ sw.js
@@ -8,10 +8,10 @@
    Compatible con PWA y notificaciones push (base)
    ============================================================ */
 
-const CACHE_STATIC = 'archinime-static-v20';
-const CACHE_DYNAMIC = 'archinime-dynamic-v20';
-const CACHE_IMAGES = 'archinime-images-v20';
-const CACHE_FONTS = 'archinime-fonts-v20';
+const CACHE_STATIC = 'archinime-static-v21';
+const CACHE_DYNAMIC = 'archinime-dynamic-v21';
+const CACHE_IMAGES = 'archinime-images-v21';
+const CACHE_FONTS = 'archinime-fonts-v21';
 
 // Recursos críticos a precachear (estáticos y siempre necesarios)
 const STATIC_ASSETS = [
