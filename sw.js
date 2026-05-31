7524be1b6db4654f7c303f42968b12aa551c2161
Archinime <archinime12@gmail.com>
2026-05-30 20:40:38 -0500

    Add files via upload

--- sw.js
+++ sw.js
@@ -3,10 +3,10 @@
    Estrategia híbrida con control absoluto sobre catálogo.js
    ============================================================ */
 
-const CACHE_STATIC = 'archinime-static-v66';
-const CACHE_DYNAMIC = 'archinime-dynamic-v66';
-const CACHE_IMAGES = 'archinime-images-v66';
-const CACHE_FONTS = 'archinime-fonts-v66';
+const CACHE_STATIC = 'archinime-static-v67';
+const CACHE_DYNAMIC = 'archinime-dynamic-v67';
+const CACHE_IMAGES = 'archinime-images-v67';
+const CACHE_FONTS = 'archinime-fonts-v67';
 
 // Recursos precacheados (catálogo NO está incluido)
 const STATIC_ASSETS = [
